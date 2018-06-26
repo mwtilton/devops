141f27d1d77426c917217ebaf33fc7ccdd09ab09
mwtilton <sandamomivo@gmail.com>
2018-06-26 11:33:26 -0700

    adding new repo for LG based scripts

---
+++ GPO.ps1
@@ -0,0 +1,5 @@
+Get-GPO -all | % {$GPOPath = "C:\GPOBackup\" + $_.DisplayName
+if (!(test-path $GPOPath)){
+    New-Item -ItemType Directory -Path $GPOPath
+}
+Backup-GPO -name $_.DisplayName -Path $GPOPath}

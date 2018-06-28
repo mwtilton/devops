31f3cf6fb6293741a3df6f8b055379399ac2abc3
mwtilton <sandamomivo@gmail.com>
2018-06-28 11:40:54 -0700

    adding in path to default xml

--- GPO.ps1
+++ GPO.ps1
@@ -1,3 +1,4 @@
+#"C:\Users\Administrator\Desktop\DefaultGPO.xml"
 Get-GPO -all | % {$GPOPath = "C:\GPOBackup\" + $_.DisplayName
 if (!(test-path $GPOPath)){
     New-Item -ItemType Directory -Path $GPOPath

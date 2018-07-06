942a924a07f0609ea74545c3941b7a070768308f
mwtilton <sandamomivo@gmail.com>
2018-07-06 09:42:04 -0700

    Creating framework for active directory stuff

--- ActiveDirectory.psm1
+++ ActiveDirectory.psm1
@@ -1,19 +1,89 @@
-Function Export-Groups {
+$PSDefaultParameterValues=@{'Write-host:BackGroundColor'='Black';'Write-host:ForeGroundColor'='Green'}
+#requires -Version 5.1
+Function Start-DCExport {
+    Param (
+        [Parameter(Mandatory=$true)]
+        [String]
+        $SrceDomain,
+        [Parameter(Mandatory=$true)]
+        [String]
+        $SrceServer,
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({Test-Path $_})]
+        [String]
+        $Path  # Working path to store files
+    )
+    Write-host "Starting Group Exports" -fore Yellow
+    Export-Groups
+} # End Function
 
+
+############################################################################
+#Export related functions
+Function Export-Groups {
+    Param (
+        [Parameter(Mandatory=$true)]
+        [String]
+        $SrceDomain,
+        [Parameter(Mandatory=$true)]
+        [String]
+        $SrceServer,
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({Test-Path $_})]
+        [String]
+        $Path  # Working path to store files
+    )
 
     $Domain = $env:USERDNSDOMAIN
     $splitDomain = $Domain.Split(".")
     $searchbase = "DC=" + $splitDomain[0] + ",DC=" + $splitDomain[1]
 
     $exportedGroups = "$env:USERPROFILE\Desktop\Exported-Groups.csv"
-    $Groups = Get-ADGroup -Properties * -Filter * -SearchBase $searchbase | select name | Export-Csv -Path $exportedGroups -NoTypeInformation
+    $Groups = Get-ADGroup -Properties * -Filter * -SearchBase $searchbase |  Export-Csv -Path $exportedGroups -NoTypeInformation
 
-    gc $exportedGroups
+    Import-Csv $exportedGroups | % {$_.name } | ft
 
 }
 
+############################################################################
+#Import related functions
+
+Function Start-DCImport {
+    Param (
+        [Parameter(Mandatory=$true,HelpMessage="Must be FQDN.")]
+        [ValidateScript({$_ -like "*.*"})]
+        [String]
+        $DestDomain,
+        [Parameter(Mandatory=$true)]
+        [String]
+        $DestServer,
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({Test-Path $_})]
+        [String]
+        $Path,
+        
+    )
+    Write-host "Starting DC Import" -fore Yellow
+    
+
+} # End Function
 
 Function Import-Groups {
 
-    
+    Param (
+        [Parameter(Mandatory=$true,HelpMessage="Must be FQDN.")]
+        [ValidateScript({$_ -like "*.*"})]
+        [String]
+        $DestDomain,
+        [Parameter(Mandatory=$true)]
+        [String]
+        $DestServer,
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({Test-Path $_})]
+        [String]
+        $Path,
+        
+    )
+
+
 }
---
+++ Call-DCExport.ps1
@@ -0,0 +1,25 @@
+<##############################################################################
+Setup
+
+##############################################################################>
+$ActiveDirectory = "$env:USERPROFILE\Desktop\ActiveDirectory"
+$ActiveDirectoryWorkingDirectory = "$ActiveDirectory\WorkingFolder"
+New-Item -ItemType Directory $ActiveDirectoryWorkingDirectory -ea SilentlyContinue
+Set-Location $ActiveDirectoryWorkingDirectory
+
+Import-Module ActiveDirectory
+Import-Module $ActiveDirectory -Force -Verbose
+
+# This path must be absolute, not relative
+$Path        = $PWD  # Current folder specified in Set-Location above
+$SrceDomain  = $env:USERDNSDOMAIN
+$SrceServer  = "$env:COMPUTERNAME.$env:USERDNSDOMAIN"
+
+Start-DCExport `
+    -SrceDomain $SrceDomain `
+    -SrceServer $SrceServer `
+    -Path $Path
+    
+###############################################################################
+# END
+###############################################################################

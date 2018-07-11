5fbf7d092c8311acf1a6723b3ceddd20328b94db
mwtilton <sandamomivo@gmail.com>
2018-07-11 07:40:53 -0700

    adding ctera project into devops repo

---
+++ ctera/C200.ps1
@@ -0,0 +1,32 @@
+Measure-Command {
+$ipaddress = Read-Host "Enter device"
+try{
+    Test-Connection $ipaddress -Quiet -ea stop
+    Write-host "$ipaddress is up" -BackgroundColor Black -ForegroundColor Green
+}
+catch{
+    Write-Warning "$ipaddress is not up and running."
+}
+
+$cred = Get-Credential
+$credUser = $cred.UserName
+$credPassword = $cred.GetNetworkCredential().password
+
+$body = (Get-Content -Path "$env:USERPROFILE\Documents\vsCode\ctera\reboot.txt")
+
+$url = "http://$ipaddress/admingui/api/login?username=$credUser&password=$credPassword"
+Invoke-RestMethod -Uri $url -Method 'post' -SessionVariable websession
+
+
+Write-Warning "Attempting reboot on $ipaddress"
+$rebooturl = "http://$ipaddress/admingui/api/status/device"
+Invoke-RestMethod -Uri $rebooturl -Method 'post' -Body $body -ContentType text/xml -WebSession $websession
+
+do{
+    "rebooting $ipaddress"
+}Until (!(Test-Connection $ipaddress -Quiet -Count 1))
+
+ping $ipaddress
+}
+
+Clear-Variable -Name credPassword -Scope Global

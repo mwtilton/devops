a7fd761d19253401f1faa7a4a6c262cc02ed85da
mwtilton <sandamomivo@gmail.com>
2018-07-19 07:43:48 -0700

    moving clear cred to after rest-method

--- ctera/C200.ps1
+++ ctera/C200.ps1
@@ -1,37 +1,44 @@
-Measure-Command {
+Function Restart-Device {
 
-Set-Location "$env:USERPROFILE\Desktop"
+    Measure-Command {
 
-$ipaddress = Read-Host "Enter device IP address"
-try{
-    Test-Connection $ipaddress -Quiet -ea stop
-    Write-host "$ipaddress is up" -BackgroundColor Black -ForegroundColor Green
+    Set-Location "$env:USERPROFILE\Desktop"
+
+    $ipaddress = Read-Host "Enter device IP address"
+    try{
+        Test-Connection $ipaddress -Quiet -ea stop
+        Write-host "$ipaddress is up" -BackgroundColor Black -ForegroundColor Green
+    }
+    catch{
+        Write-Warning "$ipaddress is not up and running."
+    }
+
+    $cred = Get-Credential
+    $credUser = $cred.UserName
+    $credPassword = $cred.GetNetworkCredential().password
+
+    $body = (Get-Content -Path ".\reboot.txt")
+
+
+    $url = "http://$ipaddress/admingui/api/login?username=$credUser&password=$credPassword"
+    Invoke-RestMethod -Uri $url -Method 'post' -SessionVariable websession
+
+
+    Write-Warning "Attempting reboot on $ipaddress"
+
+    $rebooturl = "http://$ipaddress/admingui/api/status/device"
+    Invoke-RestMethod -Uri $rebooturl -Method 'post' -Body $body -ContentType text/xml -WebSession $websession
+
+    Clear-Variable -Name credPassword -Scope Global
+
+    do{
+        "rebooting $ipaddress"
+    }Until (!(Test-Connection $ipaddress -Quiet -Count 1))
+
+    ping $ipaddress
+    }
+
+
+
 }
-catch{
-    Write-Warning "$ipaddress is not up and running."
-}
-
-$cred = Get-Credential
-$credUser = $cred.UserName
-$credPassword = $cred.GetNetworkCredential().password
-
-$body = (Get-Content -Path ".\reboot.txt")
-
-#$url = "http://$ipaddress/admingui/api/login?username=$credUser&password=$credPassword"
-$url = "http://$ipaddress/admin/api/login?username=$credUser&password=$credPassword"
-Invoke-RestMethod -Uri $url -Method 'post' -SessionVariable websession
-
-
-Write-Warning "Attempting reboot on $ipaddress"
-#$rebooturl = "http://$ipaddress/admingui/api/status/device"
-$rebooturl = "http://$ipaddress/admin/api/status/device"
-Invoke-RestMethod -Uri $rebooturl -Method 'post' -Body $body -ContentType text/xml -WebSession $websession
-
-do{
-    "rebooting $ipaddress"
-}Until (!(Test-Connection $ipaddress -Quiet -Count 1))
-
-ping $ipaddress
-}
-
-Clear-Variable -Name credPassword -Scope Global
+Restart-Device

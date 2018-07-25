c24ec3cdd03ed91e30e431478e88755d79ff56cd
mwtilton <sandamomivo@gmail.com>
2018-07-25 08:08:10 -0700

    fixing the output and count settings

--- FilesFolders/FilesFolders/FilesFolders.psm1
+++ FilesFolders/FilesFolders/FilesFolders.psm1
@@ -54,6 +54,7 @@ function Get-FilesFolders {
     Begin{
         $getFilesFolders = Get-ChildItem $path -Recurse
         $getFilesFolders | ForEach-Object {
+            <#
             $spaces = @()
             Write-Host $_.Length -ForegroundColor Cyan -NoNewline
             (1..$_.Length) | ForEach-Object {
@@ -62,7 +63,11 @@ function Get-FilesFolders {
             $spaces += "END"
             Write-Host $spaces -ForegroundColor Cyan
             Write-host ($_.FullName).Replace($path, $spaces) -ForegroundColor Red
+            #>
 
+            Write-host ($_.FullName).split("\")
+            Write-Host $path -ForegroundColor Cyan
+            Write-host ($_.FullName).Replace($path, "") -ForegroundColor Red
         }
     }
 

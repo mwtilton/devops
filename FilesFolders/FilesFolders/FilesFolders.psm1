1ba29ce41b78be18adac6211b91c69dc853db9af
mwtilton <sandamomivo@gmail.com>
2018-08-01 10:01:09 -0700

    errorhandling output to console formatting w/ invalid directory changes

--- FilesFolders/FilesFolders/FilesFolders.psm1
+++ FilesFolders/FilesFolders/FilesFolders.psm1
@@ -197,20 +197,23 @@ Function New-FileShares {
 
     $importCSV = Import-csv $csv | ? {$_.path -ne ""}
     $importcsv | Foreach-object {
-        Write-host "  [>]" -foregroundcolor DarkGray
-        Write-host "Testing Server Shares" -foregroundcolor DarkGray
-        $_
+        Write-host "  [>]" -foregroundcolor DarkGray -NoNewline
+        Write-host "Creating Shares for " -foregroundcolor DarkGray -NoNewline
+        Write-host $_.path -ForegroundColor White -NoNewline
 
-        Write-host $_.path -ForegroundColor red
         Try{
             New-item -Path $_.path -ItemType Directory -ErrorAction Stop | Out-null
+            Write-host "  [+]" -NoNewline
+            Write-host $_.Name -ForegroundColor White -NoNewline
+            Write-host " folder has been created." -ForegroundColor DarkGreen
+
         }
         Catch{
             If($_.exception.tostring().contains("already exists")){
 
             }
             elseif($_.exception.tostring().contains("A drive with the name")){
-                Write-host "  [-]" -fore red -NoNewline
+                Write-host "`r`n  [-]" -fore red -NoNewline
                 Write-host "This is probably trying to create a new folder to the drive letter " -ForegroundColor DarkYellow -nonewline
                 Write-host $_.targetobject -foregroundcolor White -nonewline
                 Write-host " and this is not possible since it is a drive letter and not a valid folder location. This can be ignored but may need to be reviewed." -ForegroundColor DarkYellow
@@ -222,16 +225,23 @@ Function New-FileShares {
         }
         Try{
             New-SmbShare –Name $_.Name -Path $_.Path –Description $_.Description -ErrorAction Stop
+            Write-host "  [+]" -NoNewline
+            Write-host $_.Name -ForegroundColor White -NoNewline
+            Write-host " is now being shared." -ForegroundColor DarkGreen
         }
         Catch{
-            If($_.exception.tostring().contains("dsafddsagfaghfdgha")){
-
+            If($_.exception.tostring().contains("already been shared")){
+                Write-Host " has already been shared. Skipped!"
             }
-            elseif($_.exception.tostring().contains("dsafddsagfaghfdgha")){
+            elseif($_.exception.tostring().contains("directory does not exist")){
+                Write-host "`r`n  [-]" -fore red -NoNewline
+                Write-host "This is probably trying to create a new share to " -ForegroundColor DarkYellow -nonewline
+                Write-host $_.Path
+                Write-host " and this is not possible since the target folder does not exist." -ForegroundColor DarkYellow
 
             }
             else{
-                Write-host "Fileshare creation error" -foregrouncolor Red
+                Write-host "Fileshare creation error" -ForegroundColor Red
                 $_ | fl * -force
                 $_.InvocationInfo.BoundParameters | fl * -force
                 $_.Exception

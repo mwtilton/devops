bb9aaafc26174c9514246537913c80c964fcd911
mwtilton <sandamomivo@gmail.com>
2018-08-14 14:19:28 -0700

    updating gppermission parameter set changes

--- tests/Export-GPPermission.Tests.ps1
+++ tests/Export-GPPermission.Tests.ps1
@@ -1,30 +1,39 @@
 Import-Module "$env:WORKINGFOLDER\DevOps\DevOps\Functions\Export-GPPermission.ps1" -Force -ErrorAction Stop
 
 Describe "Export-GPPermission" -Tags "UNIT" {
-    Mock Get-GPO {}
-    Mock Get-ADObject {}
 
     Setup -Dir "Desktop\WorkingFolder"
+
     $paramargs = @{
         SrceServer = "$env:COMPUTERNAME"
         SrceDomain = "$env:USERDNSDOMAIN"
         Path = "TestDrive:\Desktop\WorkingFolder"
     }
-    Context "Mocking assertions for errors" {
 
+    Context "Mocking assertions for errors" {
+        Mock Get-GPO {}
+        Mock Get-ADObject {}
+
+        #displayname
         It "should throw an exception" {
-            $result = Export-GPPermission
-            {$result} | Should throw
-        }
-        It "should throw when no displayname param is set" {
-            $result = Export-GPPermission @paramargs
+            $result = Export-GPPermission @paramargs -DisplayName "Accounting"
             { $result } | Should throw
         }
-    }
-    Context "Mocking Assertions for Truthsss" {
-        Mock Get-GPO {}
+        It "should throw when no displayname param is set" {
+            $result = Export-GPPermission @paramargs -all
+            { $result } | Should throw
+        }
 
-        Export-GPPermission @paramargs -all -displayname "dsa"
+        #all
+        It "" -Skip {
+
+        }
+    }
+    Context "Mocking Assertions for Truthsss"{
+        Mock Get-GPO {}
+        Mock Get-ADObject {}
+
+        Export-GPPermission @paramargs
 
         It "the get-gpo should be called twice" {
             Assert-MockCalled -CommandName Get-GPO -Exactly 2 -Scope Context

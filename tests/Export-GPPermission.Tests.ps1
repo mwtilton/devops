854dc4c6a7176ccb9f556f68c7349cf7e277573c
mwtilton <sandamomivo@gmail.com>
2018-08-16 14:23:33 -0700

    adding in exception throwing for tests with export-gp

--- tests/Export-GPPermission.Tests.ps1
+++ tests/Export-GPPermission.Tests.ps1
@@ -10,13 +10,21 @@ Describe "Export-GPPermission" -Tags "UNIT" {
         SrceDomain = "$env:USERDNSDOMAIN"
         Path = "TestDrive:\Desktop\WorkingFolder"
     }
-
+    #-DisplayName "Accounting" -SrceServer "$env:COMPUTERNAME" -SrceDomain "$env:USERDNSDOMAIN" -Path "TestDrive:\Desktop\WorkingFolder"
     Context "Mocking assertions for errors" {
-        Mock Get-GPO {return "Accounting"}
-        Mock Get-ADObject {return $true}
+        $list = "Accounting","Admin"
 
-        It "should throw when $($paramargs.SrceServer)" {
-            { Export-GPPermission @paramargs -ErrorAction Stop } | Should throw
+        Mock Get-GPO {return $list}
+        Mock Get-ADObject -MockWith {}
+
+        It "should throw when no args are set" {
+            { Export-GPPermission -ErrorAction Stop } | Should throw
+        }
+        It "throws specific exception for parameter binding exception" {
+            { Export-GPPermission -ErrorAction Stop } | Should -Throw -ExceptionType ([System.Management.Automation.ParameterBindingException])
+        }
+        It "thows specific exception for identity issue" {
+            { Export-GPPermission @paramargs -ErrorAction Stop } | Should -Not -Throw -ExceptionType ([System.Management.Automation.ParameterBindingException])
         }
     }
 

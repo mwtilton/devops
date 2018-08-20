e2fe9f3664c052cb5deb8011ca1c5d8b221f64a3
mwtilton <sandamomivo@gmail.com>
2018-08-20 09:30:20 -0700

    updating the git ignore file to include and extra excel files

--- DevOps/DevOps.psm1
+++ DevOps/DevOps.psm1
@@ -8,7 +8,7 @@ Import-Module $PSScriptRoot\DevOps.Machine.ps1 -Force -ErrorAction Stop
 $functions = Get-ChildItem $PSScriptRoot\Functions -Filter "*.ps1"
 $functions | ForEach-Object {
     #Write-Host $_.Name
-    . $PSScriptRoot\Functions\$($_.name)
+    Import-Module $PSScriptRoot\Functions\$($_.name) -Force
 }
 Export-ModuleMember *
 

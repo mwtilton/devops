ff949e6eaddae077a9941347ccdc2c41e9010613
Tilt <charlietoast04@gmail.com>
2018-08-25 22:43:03 -0700

    adding more aliases and final output of aliases added

--- SetupGit/SetupGit.sh
+++ SetupGit/SetupGit.sh
@@ -1,2 +1,12 @@
 git config --global user.name ""
 git config --global user.email ""
+
+
+git config --global alias.last "log -1 HEAD"
+git config --global alias.psu "push --set-upstream"
+git config --global alias.NUKE "reset --hard HEAD"
+git config --global alias.stashes "stash list"
+git config --global alias.s "status -s"
+#git config --global alias.prum "pull --rebase upstream master"
+
+git config --get-regexp alias

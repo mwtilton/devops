64852af66a8bbdb95cf9c286cd1e7b556afec600
Tilt <charlietoast04@gmail.com>
2018-08-25 22:47:35 -0700

    adding in commit search

--- SetupGit/SetupGit.sh
+++ SetupGit/SetupGit.sh
@@ -7,6 +7,7 @@ git config --global alias.psu "push --set-upstream"
 git config --global alias.NUKE "reset --hard HEAD"
 git config --global alias.stashes "stash list"
 git config --global alias.s "status -s"
-#git config --global alias.prum "pull --rebase upstream master"
 
 git config --get-regexp alias
+#git log — all — grep=’commit message’
+#git config --global alias.prum "pull --rebase upstream master"

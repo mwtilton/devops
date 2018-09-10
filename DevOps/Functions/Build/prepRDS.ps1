0da0a0963136f8bcd8714e1aa613c9880228d6dd
mwtilton <sandamomivo@gmail.com>
2018-09-10 11:51:18 -0700

    added new folder creation for mof DSC files on the desktop

--- DevOps/Functions/Build/prepRDS.ps1
+++ DevOps/Functions/Build/prepRDS.ps1
@@ -1,3 +1,4 @@
 Get-PackageSource -Name PSGallery | Set-PackageSource -Trusted -Force -ForceBootstrap
-
+$RDSDSCFolder = "$env:USERPROFILE\Desktop"
+New-Item -ItemType Directory -Name RDSDSC -Path $RDSDSCFolder
 Install-Module -Name xRemoteDesktopSessionHost

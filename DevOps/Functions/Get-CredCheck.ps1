20e95f47412ac4e4509455acc9bb80e71c11a10b
mwtilton <sandamomivo@gmail.com>
2018-09-17 13:28:19 -0700

    credential changing validation to simplier functionality

--- DevOps/Functions/Get-CredCheck.ps1
+++ DevOps/Functions/Get-CredCheck.ps1
@@ -1,6 +1,3 @@
-
-
-
 Function Get-CredCheck {
     [CmdletBinding()]
     param (
@@ -14,12 +11,36 @@ Function Get-CredCheck {
     # Extract the current user's domain and also pre-format the user name to be used in the credential prompt.
     $UserDomain = $env:USERDOMAIN
     $UserName = "$UserDomain\$env:USERNAME"
+
+
     # Define the starting number (always #1) and the desired maximum number of attempts, and the initial credential prompt message to use.
     $Attempt = 1
-    $MaxAttempts = 5
+    $MaxAttempts = 1
     $CredentialPrompt = "Enter your Domain account password (attempt #$Attempt out of $MaxAttempts):"
     # Set ValidAccount to false so it can be used to exit the loop when a valid account is found (and the value is changed to $True).
     $ValidAccount = $False
+    $cred = Get-Credential #Read credentials
+    $username = $cred.username
+    $password = $cred.GetNetworkCredential().password
+
+    # Get current domain using logged-on user's credentials
+    $CurrentDomain = "LDAP://" + ([ADSI]"").distinguishedName
+    $domain = New-Object System.DirectoryServices.DirectoryEntry($CurrentDomain,$UserName,$Password)
+
+    if ($domain.name -eq $null)
+    {
+        write-host "Authentication failed - please verify your username and password."
+        Break #terminate the script.
+    }
+    else
+    {
+        write-host "Successfully authenticated with domain $domain.name"
+    }
+    <#
+    $Credentials = Get-Credential -UserName $UserName -Message $CredentialPrompt
+    $ContextType = [System.DirectoryServices.AccountManagement.ContextType]::Domain
+    $ValidAccount = $PrincipalContext.ValidateCredentials($UserName,$Credentials.GetNetworkCredential().Password)
+
 
     # Loop through prompting for and validating credentials, until the credentials are confirmed, or the maximum number of attempts is reached.
     Do {
@@ -36,22 +57,28 @@ Function Get-CredCheck {
             $ContextType = [System.DirectoryServices.AccountManagement.ContextType]::Domain
             Try {
                 $PrincipalContext = New-Object System.DirectoryServices.AccountManagement.PrincipalContext $ContextType,$UserDomain
-            } Catch {
+            }
+            Catch {
                 If ($_.Exception.InnerException -like "*The server could not be contacted*") {
                     $FailureMessage = "Could not contact a server for the specified domain on attempt #$Attempt out of $MaxAttempts."
-                } Else {
+                }
+                Else {
                     $FailureMessage = "Unpredicted failure: `"$($_.Exception.Message)`" on attempt #$Attempt out of $MaxAttempts."
                 }
             }
             # If there wasn't a failure talking to the domain test the validation of the credentials, and if it fails record a failure message.
             If (-not($FailureMessage)) {
                 $ValidAccount = $PrincipalContext.ValidateCredentials($UserName,$Credentials.GetNetworkCredential().Password)
+
+                Write-Host $UserName $Credentials.GetNetworkCredential().Password "returns $ValidAccount"-ForegroundColor Red
+
                 If (-not($ValidAccount)) {
                     $FailureMessage = "Bad user name or password used on credential prompt attempt #$Attempt out of $MaxAttempts."
                 }
             }
         # Otherwise the credential prompt was (most likely accidentally) bypassed so record a failure message.
-        } Else {
+        }
+        Else {
             $FailureMessage = "Credential prompt closed/skipped on attempt #$Attempt out of $MaxAttempts."
         }
 
@@ -66,24 +93,5 @@ Function Get-CredCheck {
             }
         }
     } Until (($ValidAccount) -or ($Attempt -gt $MaxAttempts))
-
-    <#
-    $username = $cred.username
-    $password = $cred.GetNetworkCredential().password
-
-    # Get current domain using logged-on user's credentials
-    $CurrentDomain = "LDAP://" + ([ADSI]"").distinguishedName
-    $domain = New-Object System.DirectoryServices.DirectoryEntry($CurrentDomain,$UserName,$Password)
-
-    if ($domain.name -eq $null)
-    {
-        write-host "Authentication failed - please verify your username and password."
-        exit #terminate the script.
-    }
-    else
-    {
-        write-host "Successfully authenticated with domain $domain.name"
-        #do something else
-    }
     #>
 }

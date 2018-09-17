fe9e45e23b3ab6bc7e35ce33bff7709abbbfc62f
mwtilton <sandamomivo@gmail.com>
2018-09-17 10:43:38 -0700

    removing extra function information in end process area with comments

--- tests/Get-CredCheck.Tests.ps1
+++ tests/Get-CredCheck.Tests.ps1
@@ -2,9 +2,12 @@ Import-Module "$env:WORKINGFOLDER\DevOps\DevOps\Functions\Get-CredCheck.ps1" -Fo
 
 Describe "Get-CredCheck" -Tag "UNIT" {
     Context "Finds the Creds" {
+        Mock Get-Credential {$true}
 
-        It "returns shares" {
-            Should not be $null
+        Get-CredCheck
+
+        It "calls the credential once on success" {
+            Assert-MockCalled -CommandName Get-Credential -Exactly 1
         }
     }
 }

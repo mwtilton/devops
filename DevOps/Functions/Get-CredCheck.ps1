f2cdc4cc273fcf406de82ccd1a48fdddf4198389
mwtilton <sandamomivo@gmail.com>
2018-09-18 06:50:21 -0700

    reverting to original version due to loop breaking incorrectly on bad attempt

--- DevOps/Functions/Get-CredCheck.ps1
+++ DevOps/Functions/Get-CredCheck.ps1
@@ -3,106 +3,18 @@ Function Get-CredCheck {
     param (
 
     )
-
-
-    #Prompt for Credentials and verify them using the DirectoryServices.AccountManagement assembly.
+    # Prompt for Credentials and verify them using the DirectoryServices.AccountManagement assembly.
     Write-Host "Please provide your credentials so the script can continue."
-    #Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+    Add-Type -AssemblyName System.DirectoryServices.AccountManagement
     # Extract the current user's domain and also pre-format the user name to be used in the credential prompt.
     $UserDomain = $env:USERDOMAIN
-    $UserName = "$userdomain\$env:USERNAME" #"$UserDomain\$env:USERNAME"
-
-
+    $UserName = "$UserDomain\$env:USERNAME"
     # Define the starting number (always #1) and the desired maximum number of attempts, and the initial credential prompt message to use.
     $Attempt = 1
-    $MaxAttempts = 1
+    $MaxAttempts = 5
     $CredentialPrompt = "Enter your Domain account password (attempt #$Attempt out of $MaxAttempts):"
     # Set ValidAccount to false so it can be used to exit the loop when a valid account is found (and the value is changed to $True).
     $ValidAccount = $False
-    <#
-    $cred = Get-Credential -UserName $UserName -Message "Please enter your Credentials"
-
-    $username = $cred.username
-    $password = $cred.GetNetworkCredential().password
-
-    # Get current domain using logged-on user's credentials
-    $CurrentDomain = "LDAP://" + ([ADSI]"").distinguishedName
-    $domain = New-Object System.DirectoryServices.DirectoryEntry($CurrentDomain,$UserName,$Password)
-
-    if ($domain.name -eq $null)
-    {
-        write-host "Authentication failed - please verify your username and password."
-        return $null
-        Break #terminate the script.
-    }
-    else
-    {
-        write-host "Successfully authenticated with domain $($domain.name)"
-        return $cred
-    }
-    #>
-
-    Do {
-        # Blank any previous failure messages and then prompt for credentials with the custom message and the pre-populated domain\user name.
-        $FailureMessage = $Null
-        $Credentials = Get-Credential -UserName $UserName -Message $CredentialPrompt
-        #$cred = Get-Credential -UserName $UserName -Message "Please enter your Credentials"
-
-        # Verify the credentials prompt wasn't bypassed.
-        If ($Credentials) {
-            # If the user name was changed, then switch to using it for this and future credential prompt validations.
-            If ($Credentials.UserName -ne $UserName) {
-                $UserName = $Credentials.UserName
-            }
-            # Test the user name (even if it was changed in the credential prompt) and password.
-
-            #$ContextType = [System.DirectoryServices.AccountManagement.ContextType]::Domain
-            $CurrentDomain = "LDAP://" + ([ADSI]"").distinguishedName
-            Try {
-                $username = $cred.username
-                $password = $cred.GetNetworkCredential().password
-                $domain = New-Object System.DirectoryServices.DirectoryEntry($CurrentDomain,$UserName,$Password)
-            }
-            Catch {
-                If ($_.Exception.InnerException -like "*The server could not be contacted*") {
-                    $FailureMessage = "Could not contact a server for the specified domain on attempt #$Attempt out of $MaxAttempts."
-                }
-                Else {
-                    $FailureMessage = "Unpredicted failure: `"$($_.Exception.Message)`" on attempt #$Attempt out of $MaxAttempts."
-                }
-            }
-            # If there wasn't a failure talking to the domain test the validation of the credentials, and if it fails record a failure message.
-            If (-not($FailureMessage)) {
-                $ValidAccount = $domain.ValidateCredentials($UserName,$Credentials.GetNetworkCredential().Password)
-
-                If (-not($ValidAccount)) {
-                    $FailureMessage = "Bad user name or password used on credential prompt attempt #$Attempt out of $MaxAttempts."
-                }
-            }
-
-        # Otherwise the credential prompt was (most likely accidentally) bypassed so record a failure message.
-        }
-        Else {
-            $FailureMessage = "Credential prompt closed/skipped on attempt #$Attempt out of $MaxAttempts."
-        }
-
-        # If there was a failure message recorded above, display it, and update credential prompt message.
-        If ($FailureMessage) {
-            Write-Warning "$FailureMessage"
-            $Attempt++
-            If ($Attempt -lt $MaxAttempts) {
-                $CredentialPrompt = "Authentication error. Please try again (attempt #$Attempt out of $MaxAttempts):"
-            } ElseIf ($Attempt -eq $MaxAttempts) {
-                $CredentialPrompt = "Authentication error. THIS IS YOUR LAST CHANCE (attempt #$Attempt out of $MaxAttempts):"
-            }
-        }
-    } Until (($ValidAccount) -or ($Attempt -gt $MaxAttempts))
-
-    <#
-    $Credentials = Get-Credential -UserName $UserName -Message $CredentialPrompt
-    $ContextType = [System.DirectoryServices.AccountManagement.ContextType]::Domain
-    $ValidAccount = $PrincipalContext.ValidateCredentials($UserName,$Credentials.GetNetworkCredential().Password)
-
 
     # Loop through prompting for and validating credentials, until the credentials are confirmed, or the maximum number of attempts is reached.
     Do {
@@ -119,21 +31,16 @@ Function Get-CredCheck {
             $ContextType = [System.DirectoryServices.AccountManagement.ContextType]::Domain
             Try {
                 $PrincipalContext = New-Object System.DirectoryServices.AccountManagement.PrincipalContext $ContextType,$UserDomain
-            }
-            Catch {
+            } Catch {
                 If ($_.Exception.InnerException -like "*The server could not be contacted*") {
                     $FailureMessage = "Could not contact a server for the specified domain on attempt #$Attempt out of $MaxAttempts."
-                }
-                Else {
+                } Else {
                     $FailureMessage = "Unpredicted failure: `"$($_.Exception.Message)`" on attempt #$Attempt out of $MaxAttempts."
                 }
             }
             # If there wasn't a failure talking to the domain test the validation of the credentials, and if it fails record a failure message.
             If (-not($FailureMessage)) {
                 $ValidAccount = $PrincipalContext.ValidateCredentials($UserName,$Credentials.GetNetworkCredential().Password)
-
-                Write-Host $UserName $Credentials.GetNetworkCredential().Password "returns $ValidAccount"-ForegroundColor Red
-
                 If (-not($ValidAccount)) {
                     $FailureMessage = "Bad user name or password used on credential prompt attempt #$Attempt out of $MaxAttempts."
                 }
@@ -142,6 +49,8 @@ Function Get-CredCheck {
         }
         Else {
             $FailureMessage = "Credential prompt closed/skipped on attempt #$Attempt out of $MaxAttempts."
+            Write-Warning "$FailureMessage"
+            break
         }
 
         # If there was a failure message recorded above, display it, and update credential prompt message.
@@ -155,9 +64,8 @@ Function Get-CredCheck {
             }
         }
     } Until (($ValidAccount) -or ($Attempt -gt $MaxAttempts))
-    #>
 
 }
 $validcred = Get-CredCheck
 
-Enter-PSSession -Credential $validCred -ComputerName FileServer01
+#Enter-PSSession -Credential $validCred -ComputerName FileServer01

abf233ea6032f8cbcbcf1db85758f59fa04a5f27
mwtilton <sandamomivo@gmail.com>
2018-09-28 07:54:39 -0700

    adding in normal configs w/ remote options

--- Build/buildFileServer.ps1
+++ Build/buildFileServer.ps1
@@ -11,20 +11,64 @@ defines which configurations you're interested in managing.
 
 configuration buildFileServer
 {
+    Import-DscResource -ModuleName xComputerManagement -ModuleVersion 3.2.0.0
+    Import-DscResource -ModuleName xNetworking -ModuleVersion 5.4.0.0
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration -ModuleVersion 8.4.0.0
+
     Import-DscResource -ModuleName xSmbShare -ModuleVersion 2.1.0.0
     Import-DSCResource -ModuleName StorageDsc -ModuleVersion 4.1.0.0
     Import-DscResource -ModuleName cNtfsAccessControl -ModuleVersion 1.3.1
     Import-DscResource -ModuleName xPSDesiredStateConfiguration -ModuleVersion 8.4.0.0
 
-    Node $ConfigData.AllNodes.NodeName
-    {
-
+    Node $ConfigData.AllNodes.NodeName {
         LocalConfigurationManager {
             ActionAfterReboot = "ContinueConfiguration"
             ConfigurationMode = "ApplyOnly"
             DebugMode = "ForceModuleImport"
             RebootNodeIfNeeded = $true
         }
+        ForEach ($config in $Node.configs) {
+            xIPAddress NewIPAddress {
+                IPAddress = $config.IPAddressCIDR
+                InterfaceAlias = $config.InterfaceAlias
+                AddressFamily = "IPV4"
+            }
+
+            xDefaultGatewayAddress NewIPGateway {
+                Address = $config.GatewayAddress
+                InterfaceAlias = $config.InterfaceAlias
+                AddressFamily = "IPV4"
+                DependsOn = "[xIPAddress]NewIPAddress"
+            }
+
+            xDnsServerAddress PrimaryDNSClient {
+                Address        = $config.DNSAddress
+                InterfaceAlias = $config.InterfaceAlias
+                AddressFamily = "IPV4"
+                DependsOn = "[xDefaultGatewayAddress]NewIPGateway"
+            }
+
+            User Administrator {
+                Ensure = "Present"
+                UserName = "Administrator"
+                Password = $domaincredentials
+                DependsOn = "[xDnsServerAddress]PrimaryDNSClient"
+            }
+
+            xComputer ChangeNameAndJoinDomain {
+                Name = $config.ThisComputerName
+                DomainName    = $config.DomainName
+                Credential    = $domaincredentials
+                DependsOn = "[User]Administrator"
+            }
+
+
+        }
+    }
+
+    Node $ConfigData.AllNodes.NodeName.IndexOf(0)
+    {
+
 
         ForEach ($Folder in $Node.FolderStructure) {
 
@@ -103,7 +147,41 @@ $ConfigData = @{
 
 
             )
+
+            configs = @(
+
+                @{
+                    ThisComputerName = "FileServer01"
+                    InterfaceAlias = "Ethernet0"
+                    IPAddressCIDR = "192.168.1.7/24"
+                    GatewayAddress = "192.168.1.1"
+                    DNSAddress = "192.168.1.6"
+Â Â Â Â Â Â Â Â Â Â Â Â         DomainName = "democloud.local"
+                    PSDscAllowPlainTextPassword = $true
+                    PSDscAllowDomainUser = $true
+                }
+
+
+            )
+        } #end node 0
+        <#
+        @{
+            Nodename = ""
+
+            configs = @(
+                @{
+                    ThisComputerName = "APP03"
+                    InterfaceAlias = "Ethernet0"
+                    IPAddressCIDR = "192.168.1.7/24"
+                    GatewayAddress = "192.168.1.1"
+                    DNSAddress = "192.168.1.6"
+Â Â Â Â Â Â Â Â Â Â Â Â         DomainName = "democloud.local"
+                    PSDscAllowPlainTextPassword = $true
+                    PSDscAllowDomainUser = $true
+                }
+            )
         }
+        #>
     )
 }
 <#
@@ -118,8 +196,8 @@ $outputPath = "$env:USERPROFILE\Desktop\buildFileServer"
 buildFileServer -ConfigurationData $ConfigData -OutputPath $outputPath
 
 $Domain = $(($env:USERDNSDOMAIN).Split(".")[0])
-
-$Session = New-CimSession -ComputerName $ConfigData.AllNodes.Nodename -Credential "$Domain\$env:USERNAME"
+$domaincredentials = Get-Credential -UserName "$Domain\$env:USERNAME" -Message "Please enter your Domain credentials"
+$Session = New-CimSession -ComputerName $ConfigData.AllNodes.Nodename -Credential $domaincredentials
 
 Set-DSCLocalConfigurationManager -CimSession $Session -Path $outputPath -Verbose
 Start-DscConfiguration -CimSession $Session -Path $outputPath -Wait -Verbose -Force

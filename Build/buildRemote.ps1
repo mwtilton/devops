cca72fdcc8aca8e74a0025a28708122aedf243c8
mwtilton <sandamomivo@gmail.com>
2018-09-28 09:24:04 -0700

    setting up remote integration testing

--- Build/buildRemote.ps1
+++ Build/buildRemote.ps1
@@ -1,4 +1,5 @@
-﻿Configuration ConfigureRebootOnNode
+﻿<#
+Configuration ConfigureRebootOnNode
 {
     param (
         [Parameter(Mandatory=$true)]
@@ -29,7 +30,7 @@ Set-DscLocalConfigurationManager -CimSession $cim -Path .\rebootMofs -Verbose
 
 # read the config settings back to confirm
 Get-DscLocalConfigurationManager -CimSession $cim
-
+#>
 
 <#
 <# Notes:
@@ -41,10 +42,12 @@ Disclaimer - This example code is provided without copyright and AS IS.  It is f
 Specify the configuration to be applied to the server.  This section
 defines which configurations you're interested in managing.
 
-
-configuration buildFileServer
+#>
+configuration buildAPPServer
 {
-    Import-DscResource -ModuleName xSmbShare -ModuleVersion 2.1.0.0
+    Import-DscResource -ModuleName xComputerManagement -ModuleVersion 4.1.0.0
+    Import-DscResource -ModuleName xNetworking -ModuleVersion 5.4.0.0
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration -ModuleVersion 8.4.0.0
 
     Node $ConfigData.AllNodes.NodeName
     {
@@ -56,78 +59,57 @@ configuration buildFileServer
             RebootNodeIfNeeded = $true
         }
 
-        ForEach ($config in $Node.configs) {
-            xIPAddress NewIPAddress {
-                IPAddress = $node.IPAddressCIDR
-                InterfaceAlias = $node.InterfaceAlias
-                AddressFamily = "IPV4"
-            }
-
-            xDefaultGatewayAddress NewIPGateway {
-                Address = $node.GatewayAddress
-                InterfaceAlias = $node.InterfaceAlias
-                AddressFamily = "IPV4"
-                DependsOn = "[xIPAddress]NewIPAddress"
-            }
-
-            xDnsServerAddress PrimaryDNSClient {
-                Address        = $node.DNSAddress
-                InterfaceAlias = $node.InterfaceAlias
-                AddressFamily = "IPV4"
-                DependsOn = "[xDefaultGatewayAddress]NewIPGateway"
-            }
-
-            User Administrator {
-                Ensure = "Present"
-                UserName = "Administrator"
-                Password = $credentials
-                DependsOn = "[xDnsServerAddress]PrimaryDNSClient"
-            }
-
-            xComputer ChangeNameAndJoinDomain {
-                Name = $node.ThisComputerName
-                DomainName    = $node.DomainName
-                Credential    = $credentials
-                DependsOn = "[User]Administrator"
-            }
-
-
+        xIPAddress NewIPAddress {
+            IPAddress = $node.IPAddressCIDR
+            InterfaceAlias = $node.InterfaceAlias
+            AddressFamily = "IPV4"
         }
 
+        xDefaultGatewayAddress NewIPGateway {
+            Address = $node.GatewayAddress
+            InterfaceAlias = $node.InterfaceAlias
+            AddressFamily = "IPV4"
+            DependsOn = "[xIPAddress]NewIPAddress"
+        }
 
+        xDnsServerAddress PrimaryDNSClient {
+            Address        = $node.DNSAddress
+            InterfaceAlias = $node.InterfaceAlias
+            AddressFamily = "IPV4"
+            DependsOn = "[xDefaultGatewayAddress]NewIPGateway"
+        }
+
+        User Administrator {
+            Ensure = "Present"
+            UserName = "Administrator"
+            Password = $credentials
+            DependsOn = "[xDnsServerAddress]PrimaryDNSClient"
+        }
+
+        xComputer ChangeNameAndJoinDomain {
+            Name = $node.ThisComputerName
+            DomainName    = $node.DomainName
+            Credential    = $domaincredentials
+            DependsOn = "[User]Administrator"
+        }
     }
 }
 <#
 Specify values for the configurations you're interested in managing.
 See in the configuration above how variables are used to reference values listed here.
-
+#>
 $ConfigData = @{
     AllNodes = @(
         @{
-
-            Nodename = "FileServer01"
-
-            configs = @(
-
-                @{
-                    Path =   "E:\Testing"
-                    Ensure = "Present"
-                }
-
-
-            )
-        }
-        @{
-            NodeName = "APP01"
-            configs = @(
-
-                @{
-                    Path =   "E:\Testing"
-                    Ensure = "Present"
-                }
-
-
-            )
+            NodeName = ""
+            ThisComputerName = "APP03"
+            InterfaceAlias = "Ethernet0"
+            IPAddressCIDR = "192.168.1.7/24"
+            GatewayAddress = "192.168.1.1"
+            DNSAddress = "192.168.1.6"
+Â Â Â Â Â Â Â Â Â Â Â Â DomainName = "democloud.local"
+            PSDscAllowPlainTextPassword = $true
+            PSDscAllowDomainUser = $true
         }
     )
 }
@@ -135,18 +117,18 @@ $ConfigData = @{
 Lastly, prompt for the necessary username and password combinations, then
 compile the configuration, and then instruct the server to execute that
 configuration against the settings on this local server.
-
+#>
 
 #$outputPath = "\\$($ConfigData.AllNodes.Nodename)\c$\users\administrator\Desktop\buildFileServer\"
-$outputPath = "$env:USERPROFILE\Desktop\buildFileServer"
+$outputPath = "$env:USERPROFILE\Desktop\buildAPPServer"
 
-buildFileServer -ConfigurationData $ConfigData -OutputPath $outputPath
+buildAPPServer -ConfigurationData $ConfigData -OutputPath $outputPath
 
 $Domain = $(($env:USERDNSDOMAIN).Split(".")[0])
+[PScredential]$domaincredentials = Get-Credential -UserName $Domain\$env:USERNAME -Message "Please enter your Domain credentials"
+[PScredential]$credentials = Get-Credential -UserName $env:USERNAME -Message "Please enter your Local Admin credentials"
 
-$Session = New-CimSession -ComputerName $ConfigData.AllNodes.Nodename -Credential "$Domain\$env:USERNAME"
+$Session = New-CimSession -ComputerName $ConfigData.AllNodes.Nodename -Credential $domaincredentials
 
 Set-DSCLocalConfigurationManager -CimSession $Session -Path $outputPath -Verbose
 Start-DscConfiguration -CimSession $Session -Path $outputPath -Wait -Verbose -Force
-
-#>

262d161d896ce350dcf37745c4943fdaafa82dcd
mwtilton <sandamomivo@gmail.com>
2018-11-26 13:38:20 -0800

    cleaning up configurations and extra comments

--- VMWare/Get-VmWareConfigs.ps1
+++ VMWare/Get-VmWareConfigs.ps1
@@ -1,2 +1,17 @@
+<#
 Set-PowerCLIConfiguration -DefaultVIServerMode multiple -Confirm:$false -scope user
 Set-PowerCLIConfiguration -DefaultVIServerMode Single -Confirm:$false -scope session
+
+Set-PowerCLIConfiguration -InvalidCertificateAction ignore -confirm:$false -Scope AllUsers
+#>
+
+Get-PowerCLIConfiguration
+Get-PowerCLIVersion
+
+#Get-Folder -Server $server -Name Folder
+
+#Get-ExternalNetwork | Select Name | ft
+#Get-Folder -Name Folder
+
+Write-host "Open Connections"
+Write-host $global:defaultviserver -ForegroundColor Red
--- VMWare/Start-Testing.ps1
+++ VMWare/Start-Testing.ps1
@@ -1,31 +1,12 @@
 Import-Module .\vmware.machine.ps1 -Force -ErrorAction Stop
 
-<#
-Get-Folder -Server $server -Name Folder
-
-Get-ExternalNetwork | Select Name | ft
-Get-Folder -Name Folder
-
-Get-PowerCLIConfiguration
-Get-PowerCLIVersion
-
-$global:defaultviserver
-#>
-Get-PowerCLIConfiguration
-$global:defaultviserver
-
 $PWord = ConvertTo-SecureString -String $vmware.password -AsPlainText -Force
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $vmware.UserName, $PWord
-#$vCenterCred = Get-Credential -Message "vCenter server or esxi credentials" -UserName $vmware.UserName
 
-Connect-viServer -server $vmware.server -Credential $Credential
-
+Connect-ViServer -server $vmware.server -Credential $Credential
+$global:defaultviserver
 
 [math]::Round(((get-vm | Where-object{$_.PowerState -eq "PoweredOn" }).UsedSpaceGB | measure-Object -Sum).Sum)
 [math]::Round(((get-vm | where-object{$_.PowerState -eq "PoweredOn" }).MemoryGB | Measure-Object -Sum).Sum ,0)
 
-<#
-Disconnect-CiServer * -Confirm:$false -Force
-
-$global:defaultviserver
-#>
+Disconnect-ViServer $global:defaultviserver -Confirm:$false -Force

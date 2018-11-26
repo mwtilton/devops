bd1f78e5dab199995cf45a49e135706d7c26a24d
mwtilton <sandamomivo@gmail.com>
2018-11-26 12:26:03 -0800

    fixing NIC filenames

--- VMWare/Get-AllOVDCs.ps1
+++ VMWare/Get-AllOVDCs.ps1
@@ -12,4 +12,14 @@ Connect-CIServer myvcloud.scalematrix.com
 Get-Folder -Name Folder
 #>
 
-Connect-VIServer -Menu
+#Connect-CIServer "10.20.0.4"
+#Connect-CIServer myvcloud.scalematrix.com
+
+Add-PSSnapin VMware.VimAutomation.core
+Add-PSSnapin VMware.VimAutomation.Vds
+ $vCenterCred = Get-Credential -Message "vCenter server or esxi credentials"
+ $vcenterServer = "10.20.0.4"
+ #myvcloud.scalematrix.com
+ Connect-viServer -server $vcenterServer -Credential $vCenterCred
+
+#[math]::Round(((get-vm | Where-object{$_.PowerState -eq "PoweredOn" }).UsedSpaceGB | measure-Object -Sum).Sum)

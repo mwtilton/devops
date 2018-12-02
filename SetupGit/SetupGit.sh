34b2fe7ded1deb9a2dc16db387411e4e4a243cab
mwtilton <sandamomivo@gmail.com>
2018-12-02 12:50:34 -0800

    adding in read prompts for user information

--- SetupGit/SetupGit.sh
+++ SetupGit/SetupGit.sh
@@ -1,8 +1,14 @@
-git config --global --unset-all alias
-git config --global --unset-all core
+# error: key does not contain a section: alias
+#git config --global --unset-all alias
+#git config --global --unset-all core
 
-git config --global user.name ""
-git config --global user.email ""
+echo "Enter Username: "
+read name
+git config --global user.name "$name"
+
+echo "Enter Email: "
+read email
+git config --global user.email "$email"
 
 git config --global alias.last "log -1 HEAD"
 git config --global alias.psu "push --set-upstream"

1460af084c9194717b07be9ed8719f183bb36700
mwtilton <charlietoast04@gmail.com>
2020-03-24 18:13:12 -0700

    fixing read name (squash me)

--- SetupGit/SetupGit.sh
+++ SetupGit/SetupGit.sh
@@ -1,5 +1,14 @@
-﻿git config --global user.name ""
-git config --global user.email ""
+﻿# error: key does not contain a section: alias
+#git config --global --unset-all alias
+#git config --global --unset-all core
+
+echo "Enter Username: "
+read name
+git config --global user.name "$name"
+
+echo "Enter Email: "
+read email
+git config --global user.email "$email"
 
 git config --global core.autocrlf true
 git config --global alias.last "log -1 HEAD"
